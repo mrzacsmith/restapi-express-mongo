3a6c6627a84d6c98d0ef96654fefc2dbdc5615c2
Zac Smith <zrs3141592@gmail.com>
2020-08-10 15:48:50 -0600

    add status codes

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,13 +8,20 @@ router.get('/', (req, res) => {
   })
 })
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   const { username } = req.body
+  if (!username) {
+    return res.status(400).send({ error: 'Request fields not found: username' })
+  }
+
   const newUser = new User({
     username: username,
   })
-  newUser.save((err, model) => {
-    return res.send(model)
+  newUser.save((error, model) => {
+    if (error) {
+      return res.status(400).send({ error })
+    }
+    return res.status(201).send(model)
   })
 })
 
