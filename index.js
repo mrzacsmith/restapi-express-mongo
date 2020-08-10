6b99e62a6cda734c059fec07027b292c685b29b5
Zac Smith <zrs3141592@gmail.com>
2020-08-09 22:47:16 -0600

    set up mongo db connection

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-import nodemon from 'nodemon'
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
+mongoose.connect(
+  'mongodb+srv://user:test123@rest-api.seovb.mongodb.net/?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, data) => {
+    if (err) {
+      console.log('DB connection failed')
+      return
+    }
+    console.log('DB connection successful')
+  }
+)
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  return res.send('Hello Express')
+})
+
+app.get('/world', (req, res) => {
+  return res.send('world')
+})
+
+app.post('/', (req, res) => {
+  return res.send(req.body)
+})
+
+const PORT = process.env.PORT || 5566
+
+app.listen(PORT, () => {
+  console.log(`\n** Servcer is listening on port ${PORT} **\n`)
+})
