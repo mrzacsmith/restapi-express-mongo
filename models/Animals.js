7d9287e874672695fefb2013143fdad20ef9159c
Zac Smith <zrs3141592@gmail.com>
2020-08-11 13:44:41 -0600

    create Animal model

---
+++ models/Animals.js
@@ -0,0 +1,12 @@
+import { Schema, mongo } from 'mongoose'
+import mongoose from '../database/mongodb.js'
+
+const animalSchema = new Schema({
+  type: { type: String, required: true },
+  name: { type: String, required: true },
+  owner: { type: Schema.Types.ObjectId, ref: 'user' },
+})
+
+const Animal = mongoose.model('animal', animalSchema)
+export default Animal
+export { animalSchema }
