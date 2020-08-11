43ffb6dd80f41d1578d30dd0213c54afe55db784
Zac Smith <zrs3141592@gmail.com>
2020-08-10 20:20:51 -0600

    fix User

--- models/Users.js
+++ models/Users.js
@@ -1,35 +1,40 @@
 import { Schema } from 'mongoose'
-import bcrypt from 'bcryptjs'
-import mongoose from '../database/mongodb.js'
+import mongoose from '../database/mongodb'
+import bcrypt from 'bcrypt'
+import _ from 'lodash'
 
 const userSchema = new Schema({
   username: { type: String, required: true, index: { unique: true } },
-  password: { type: String, require: true },
+  password: { type: String, required: true },
+  animals: [{ type: Schema.Types.ObjectId, ref: 'animal' }],
 })
 
-const upatePassword = (user, next) => {
-  bcrypt.genSalt(10, (error, salt) => {
-    if (error) return next(error)
+function updatePassword(user, next) {
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err)
 
-    bcrypt.hashSync(user.password, salt, (error, hash) => {
-      if (error) return next(error)
-
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err)
       user.password = hash
       next()
     })
   })
 }
 
-userSchema.pre('save', () => {
-  upatePassword(this, next)
+userSchema.pre('save', function (next) {
+  updatePassword(this, next)
 })
 
-userSchema.methods.comparePassword = (password, next) => {
-  bcrypt.compare(password, this.password, (err, isMatch) => {
-    if (error) return next(error)
+userSchema.methods.comparePassword = function (password, next) {
+  bcrypt.compare(password, this.password, function (err, isMatch) {
+    if (err) return next(err)
 
     return next(null, isMatch)
   })
+}
+
+userSchema.methods.removePass = function () {
+  return _.omit(this.toObject(), 'password')
 }
 
 export default mongoose.model('user', userSchema)
